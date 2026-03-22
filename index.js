18ec3c3ecfecfaf97521e69d57bd46714f4e1ca5
Swati-cod <Bhumiharswati43@gmail.com>
2026-03-22 10:08:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function checkEmergency(text) {
   text = text.toLowerCase();
 
   if (text.includes("unconscious") || text.includes("bleeding") || text.includes("heart attack")) {
-    return "🔴 Critical - Immediate medical attention required";
+    return "Critical - Immediate medical attention required";
   } else if (text.includes("fever") || text.includes("fracture")) {
-    return "🟡 Moderate - Needs medical attention soon";
+    return "Moderate - Needs medical attention soon";
   } else {
-    return "🟢 Low - Minor issue";
+    return " Low - Minor issue";
   }
 }
 
